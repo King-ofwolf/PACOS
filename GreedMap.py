4bd45ee403b810bc67c943fe8ebf5bb2e2d0bb3a
wangshengling <wangshengling897@163.com>
2018-11-22 17:36:11 +0800

    add

--- GreedMap.py
+++ GreedMap.py
@@ -9,7 +9,7 @@
 __author__ = 'Wang'
 
 from functools import partial
-
+from random import randint
 def GreedMap(Gt,Nt,T,packNodeFirst,cost_function):
 	#initiallize
 	S=[-1 for i in range(Nt.node_size*Nt.core_size)]
@@ -21,16 +21,29 @@ def GreedMap(Gt,Nt,T,packNodeFirst,cost_function):
 			pass
 		else:
 			#for every node in closest_free_nodes get free cores
+			K=[]
+			cores=[]
 			for pnode in closest_free_node(Nt,S,node_last):
 				for pcore in get_node_free_cores(Nt,S,pnode):
 					cores.append(pcore)	
 			#for every usable core, caculate cost and choose the mincost cores add to K
+			cores_cost=[]
 			for pcore in cores:
 				S_temp=S[:]
 				S_temp[pcore]=t 	#try to map task t to pcore
-				
-
-
+				cores_cost.append((pcore,cost_function(Gt,Nt,S_temp,t)))
+			cores_cost_sorted=sorted(cores_cost,key=lambda x:x[1])	#sorted by cores cost 
+			mincost=cores_cost_sorted[0][1]
+			for pcore_cost in cores_cost_sorted:
+				if pcore_cost[1]==mincost:
+					K.append(pcore_cost[0])
+				else:
+					break
+		pcore=K[randint(0,len(K)-1)]
+		S[pcore]=t
+		node_last=Nt.getnodeoder(pcore)
+	return S
+	
 def closest_free_node(Nt,S,pnode):
 	nodes=[]
 	mindistance=Nt.getnodedistance(pnode,0)
